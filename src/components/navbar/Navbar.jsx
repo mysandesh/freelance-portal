dd11f035a1ef6f567c173fc181be6968da60a331
Sandesh Acharya <mysandesh@gmail.com>
2023-03-03 12:02:16 +0545

    Added Navbar Design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">Freelance Portal</span>
+          <span className="dot">.</span>
+          {/* </Link> */}
+        </div>
+        <div className="links">
+          <span>Freelance Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      <hr />
+      <div className="menu">
+        <span>Test</span>
+        <span>Test</span>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
