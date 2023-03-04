74d96760f71431ee637921e82ed979a7a3cad539
Sandesh Acharya <mysandesh@gmail.com>
2023-03-04 18:41:03 +0545

    Navbar Scroll Effect Added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
 
   return (
     <div className={active ? "navbar active" : "navbar"}>
