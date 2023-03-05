0eb5a041b6ee6b078e20bf267c6b9c19de3823e9
Sandesh Acharya <mysandesh@gmail.com>
2023-03-05 10:55:00 +0545

    Navbar Scroll Transition Effect Added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ function App() {
   return (
     <div>
       <Navbar />
+      <h1>
+        There are many variations of passages available, but the majority have
+        suffered alteration in some form, by injected humour, or randomised
+        words which don't look even slightly believable.
+      </h1>
     </div>
   );
 }
