99cd3d7293829860997dda66b468bc51bccbd465
Sandesh Acharya <mysandesh@gmail.com>
2023-03-06 11:45:36 +0545

    React Router DOM Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
