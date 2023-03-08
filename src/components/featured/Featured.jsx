b38caa8423b3e0c64eb95f5991d49bc2d91f3d40
Sandesh Acharya <mysandesh@gmail.com>
2023-03-08 20:50:01 +0545

    Featured Component Design Initiated

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Featured.scss";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="container">Featured Test</div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Home.scss";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Featured />
+    </div>
+  );
 };
 
 export default Home;
