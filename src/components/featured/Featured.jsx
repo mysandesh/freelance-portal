25005f76e77ab2294c634b6b6d39db963adac14a
Sandesh Acharya <mysandesh@gmail.com>
2023-03-09 21:26:48 +0545

    Featured Component Design Done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,7 +6,9 @@ const Featured = () => {
     <div className="featured">
       <div className="container">
         <div className="left">
-          <h1>Find the perfect freelance services for your business</h1>
+          <h1>
+            Find the perfect <i>freelance</i> services for your business
+          </h1>
           <div className="search">
             <div className="searchInput">
               <img src="./public/img/search.png" alt="" />
