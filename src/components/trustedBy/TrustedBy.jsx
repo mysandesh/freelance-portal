ffc9ed3b83eb1f2b702a24236907e357027de4f0
Sandesh Acharya <mysandesh@gmail.com>
2023-03-10 20:54:37 +0545

    TrustedBy Component Design Initiated

---
+++ src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TrustedBy.scss";
+
+const TrustedBy = () => {
+  return <div className="trustedBy">TrustedBy</div>;
+};
+
+export default TrustedBy;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.scss";
 import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
+      <TrustedBy />
     </div>
   );
 };
