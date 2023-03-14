5b46f65d486a4ba81046f442d13ee46d7d9152c3
Sandesh Acharya <mysandesh@gmail.com>
2023-03-14 21:57:28 +0545

    Category Card imported to Homepage

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -5,7 +5,25 @@ import Slider from "infinite-react-carousel";
 const Slide = () => {
   return (
     <div className="slide">
-      <div className="container"></div>
+      <div className="container">
+        <Slider>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+        </Slider>
+      </div>
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.scss";
 import Featured from "../../components/featured/Featured";
+import Slide from "../../components/Slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div className="home">
       <Featured />
       <TrustedBy />
+      <Slide />
     </div>
   );
 };
