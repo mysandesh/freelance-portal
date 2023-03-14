f1b14bb57e6b55fe46b01f2a03781ddf725b7e88
Sandesh Acharya <mysandesh@gmail.com>
2023-03-14 23:57:54 +0545

    Category Slide Implementation

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -2,26 +2,12 @@ import React from "react";
 import "./Slide.scss";
 import Slider from "infinite-react-carousel";
 
-const Slide = () => {
+const Slide = ({ children, slidesToShow, arrowsScroll }) => {
   return (
     <div className="slide">
       <div className="container">
-        <Slider>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
+        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+          {children}
         </Slider>
       </div>
     </div>
