eb0d7b26fa1b6a31a5c6b2e03994d49c090cd36e
Sandesh Acharya <mysandesh@gmail.com>
2023-03-14 21:12:09 +0545

    Infinite React Category Card initiated

---
+++ src/components/catCard/CatCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CatCard.scss";
+
+const CatCard = () => {
+  return (
+    <div className="catCard">
+      <div className="container"></div>
+    </div>
+  );
+};
+
+export default CatCard;
