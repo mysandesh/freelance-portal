905544681734af007e3b771ebbecce32d3700ec2
Sandesh Acharya <mysandesh@gmail.com>
2023-03-14 23:58:54 +0545

    Category Infinite Slider Completed

--- src/components/catCard/CatCard.jsx
+++ src/components/catCard/CatCard.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./CatCard.scss";
 
-const CatCard = ({ item }) => {
+function CatCard({ card }) {
   return (
     <Link to="/gigs?cat=design">
       <div className="catCard">
-        <img src="{item.img}" alt="" />
-        <span className="desc">{item.desc}</span>
-        <span className="title">{item.title}</span>
+        <img src={card.img} alt="" />
+        <span className="desc">{card.desc}</span>
+        <span className="title">{card.title}</span>
       </div>
     </Link>
   );
-};
-
+}
 export default CatCard;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Home.scss";
 import Featured from "../../components/featured/Featured";
-import Slide from "../../components/Slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/Slide/Slide";
+import CatCard from "../../components/catCard/CatCard";
+import { cards } from "../../data";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
       <TrustedBy />
-      <Slide />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
