512451c8eefc0a0fd82b4710b8022b2c7034b198
Sandesh Acharya <mysandesh@gmail.com>
2023-03-16 21:40:04 +0545

    Features Title Text added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -21,7 +21,13 @@ const Home = () => {
           <div className="item">
             <h1>A whole world of freelance talent at your fingertips</h1>
             <div className="title"></div>
+            <img src="./img/check.png" alt="" />
+            The best for every budget
           </div>
+          <p>
+            Find high-quality services at every price point. No hourly rates,
+            just project-based pricing.
+          </p>
           <div className="item">
             <video src="./img/video.mp4" controls></video>
           </div>
