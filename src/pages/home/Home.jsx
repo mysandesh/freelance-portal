371db7195be39108430900e04bc19c172d3936a3
Sandesh Acharya <mysandesh@gmail.com>
2023-03-16 23:06:59 +0545

    Features Sections added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,40 +20,41 @@ const Home = () => {
         <div className="container">
           <div className="item">
             <h1>A whole world of freelance talent at your fingertips</h1>
-            <div className="title"></div>
-            <img src="./img/check.png" alt="" />
-            The best for every budget
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Quality work done quickly
+            </div>
+            <p>
+              Find the right freelancer to begin working on your project within
+              minutes.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Protected payments, every time
+            </div>
+            <p>
+              Always know what you'll pay upfront. Your payment isn't released
+              until you approve the work.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              24/7 support
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
           </div>
-          <p>
-            Find high-quality services at every price point. No hourly rates,
-            just project-based pricing.
-          </p>
-          <div className="title">
-            <img src="./img/check.png" alt="" />
-            Quality work done quickly
-          </div>
-          <p>
-            Find the right freelancer to begin working on your project within
-            minutes.
-          </p>
-          <div className="title">
-            <img src="./img/check.png" alt="" />
-            Protected payments, every time
-          </div>
-          <p>
-            Always know what you'll pay upfront. Your payment isn't released
-            until you approve the work.
-          </p>
-          <div className="title">
-            <img src="./img/check.png" alt="" />
-            24/7 support
-          </div>
-          <p>
-            Find high-quality services at every price point. No hourly rates,
-            just project-based pricing.
-          </p>
           <div className="item">
-            <video src="./img/video.mp4" controls></video>
+            <video src="./img/video.mp4" controls />
           </div>
         </div>
       </div>
