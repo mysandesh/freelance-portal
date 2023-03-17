2ac45cfe59eb74e1e6bf34f4faf83faaa04bb01c
Sandesh Acharya <mysandesh@gmail.com>
2023-03-17 13:39:17 +0545

    Footer Categories Added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,66 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="container">
-        <div className="top"></div>
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>About</h2>
+            <span>Press & News</span>
+            <span>Partnerships</span>
+            <span>Privacy Policy</span>
+            <span>Terms of Service</span>
+            <span>Intellectual Property Claims</span>
+            <span>Investor Relations</span>
+            <span>Contact Sales</span>
+          </div>
+          <div className="item">
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+            <span>Selling on Liverr</span>
+            <span>Buying on Liverr</span>
+          </div>
+          <div className="item">
+            <h2>Community</h2>
+            <span>Customer Success Stories</span>
+            <span>Community hub</span>
+            <span>Forum</span>
+            <span>Events</span>
+            <span>Blog</span>
+            <span>Influencers</span>
+            <span>Affiliates</span>
+            <span>Podcast</span>
+            <span>Invite a Friend</span>
+            <span>Become a Seller</span>
+            <span>Community Standards</span>
+          </div>
+          <div className="item">
+            <h2>More From Fiverr</h2>
+            <span>Liverr Business</span>
+            <span>Liverr Pro</span>
+            <span>Liverr Logo Maker</span>
+            <span>Liverr Guides</span>
+            <span>Get Inspired</span>
+            <span>Liverr Select</span>
+            <span>ClearVoice</span>
+            <span>Liverr Workspace</span>
+            <span>Learn</span>
+            <span>Working Not Working</span>
+          </div>
+        </div>
         <div className="bottom">
           <div className="left"></div>
           <div className="right"></div>
