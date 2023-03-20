dd139460ba19b88036c30148d9a666daa6dfd924
Sandesh Acharya <mysandesh@gmail.com>
2023-03-20 22:20:49 +0545

    Item Image and Username added on Category page

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -7,7 +7,12 @@ const GigCard = ({ item }) => {
       <div className="gigCard">
         <div className="gigCard">
           <img src="{item.img}" alt="" />
-          <div className="info"></div>
+          <div className="info">
+            <div className="user">
+              <img src="{item.pp}" alt="" />
+              <span>{item.username}</span>
+            </div>
+          </div>
           <div className="details"></div>
         </div>
       </div>
