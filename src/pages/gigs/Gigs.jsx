ecc6487f91b1d6842902cb9af2700b1e4f92e752
Sandesh Acharya <mysandesh@gmail.com>
2023-03-20 23:04:44 +0545

    Categories page design intiated

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./Gigs.scss";
 
 const Gigs = () => {
-  return <div>Gigs</div>;
+  return (
+    <>
+      <div className="gigs">
+        <div className="container">
+          <span className="breadcrumbs">FIVERR > GRAPHICS & DESIGN > </span>
+          <h1>AI Artists</h1>
+          <p>
+            Explore the boundaries of art and technology with Fiverr's AI
+            Artists
+          </p>
+          <div className="menu">
+            <div className="left"></div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Gigs;
