869c5df8fc0f525c422aa881fa1f459f93cf43f4
Sandesh Acharya <mysandesh@gmail.com>
2023-03-25 23:54:10 +0545

    Single Product Page Design Complete

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -242,6 +242,39 @@ const Gig = () => {
             <h3>1 AI generated image</h3>
             <h2>$ 59.99</h2>
           </div>
+          <p>
+            I will create a unique high quality AI generated image based on a
+            description that you give me
+          </p>
+          <div className="details">
+            <div className="item">
+              <img src="/img/clock.png" alt="" />
+              <span>2 Days Delivery</span>
+            </div>
+            <div className="item">
+              <img src="/img/recycle.png" alt="" />
+              <span>3 Revisions</span>
+            </div>
+          </div>
+          <div className="features">
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Prompt writing</span>
+            </div>
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Artwork delivery</span>
+            </div>
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Image upscaling</span>
+            </div>
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Additional design</span>
+            </div>
+          </div>
+          <button>Continue</button>
         </div>
       </div>
     </div>
