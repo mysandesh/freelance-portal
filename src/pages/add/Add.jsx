43bd87afad5b80a6539a7a3565bd1ff75586f508
Sandesh Acharya <mysandesh@gmail.com>
2023-03-26 21:43:55 +0545

    Add Gig page sections added

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./Add.scss";
 
 const Add = () => {
-  return <div className="add">Add</div>;
+  return (
+    <div className="add">
+      <div className="container">
+        <h1>Add New Gig</h1>
+        <div className="sections">
+          <div className="left">
+            <label htmlFor="">Title</label>
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
