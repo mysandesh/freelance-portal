840e14fee86dfc78ad5fc9f27664bda7ae369509
Sandesh Acharya <mysandesh@gmail.com>
2023-03-26 22:41:39 +0545

    UI Part relocated to client folder

---
+++ client/src/pages/orders/Orders.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import "./Orders.scss";
+import { Link } from "react-router-dom";
+
+const Orders = () => {
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true,
+  };
+  return (
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Imagine</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Stunning concept art</td>
+            <td>
+              59.<sup>99</sup>
+            </td>
+            <td>13</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>
+              120.<sup>99</sup>
+            </td>
+            <td>41</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>High quality digital character</td>
+            <td>
+              79.<sup>99</sup>
+            </td>
+            <td>55</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>
+              119.<sup>99</sup>
+            </td>
+            <td>29</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>
+              59.<sup>99</sup>
+            </td>
+            <td>34</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Text based ai generated art</td>
+            <td>
+              110.<sup>99</sup>
+            </td>
+            <td>16</td>
+            <td>
+              <img className="delete" src="./img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Register.scss";
+
+const Register = () => {
+  return <div>Register</div>;
+};
+
+export default Register;
