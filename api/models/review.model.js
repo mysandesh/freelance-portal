91f673c0e209842cce760a77bcec9ee09cc3eac8
Sandesh Acharya <mysandesh@gmail.com>
2023-04-02 12:05:55 +0545

    Review Model Schema completed

---
+++ api/models/review.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const ReviewSchema = new Schema(
+  {
+    gigID: {
+      type: String,
+      required: true,
+    },
+    userID: {
+      type: String,
+      required: true,
+    },
+    star: {
+      type: Number,
+      required: true,
+      enum: [1, 2, 3, 4, 5],
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Review", ReviewSchema);
