acc3b0042c533bc450377c408ae532c18cfe7aa6
Sandesh Acharya <mysandesh@gmail.com>
2023-04-06 22:16:03 +0545

    Routes added on Server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
 
 const app = express();
 dotenv.config();
@@ -18,6 +23,11 @@ const connect = async () => {
 };
 
 app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
 
 app.listen(8800, () => {
   connect();
