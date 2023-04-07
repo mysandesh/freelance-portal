026096f095d9df2a6612e10d0b1b87ef936c74a6
Sandesh Acharya <mysandesh@gmail.com>
2023-04-07 22:05:15 +0545

    Auth Route post method fixed

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/register");
-router.get("/login");
+router.post("/register");
+router.post("/login");
 
 export default router;
