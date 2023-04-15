13b441cecddf4edeb24277046a18e05cee4df2bb
Sandesh Acharya <mysandesh@gmail.com>
2023-04-15 22:07:17 +0545

    Cookie Parser imported on server side

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ import orderRoute from "./routes/order.route.js";
 import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -24,6 +25,7 @@ const connect = async () => {
 };
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
