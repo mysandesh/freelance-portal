fcb7f13b834657f6bb132d786238b2770250889a
Sandesh Acharya <mysandesh@gmail.com>
2023-04-17 23:11:33 +0545

    Controller import bug fixed

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,1 +1,15 @@
-export const fn = (req, res) => {};
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).send("You are not authenticated!");
+
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (payload.id !== user._id) {
+      return res.status(403).send("You can delete only your account!");
+    }
+    await User.findByIdAndDelete(req.params.id);
+  });
+};
