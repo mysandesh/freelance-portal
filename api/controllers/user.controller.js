857a65e394962e1d5a51427cdf4123274b6c72c6
Sandesh Acharya <mysandesh@gmail.com>
2023-04-18 20:14:43 +0545

    User credential converted to string

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,7 +7,7 @@ export const deleteUser = async (req, res) => {
   if (!token) return res.status(401).send("You are not authenticated!");
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    if (payload.id !== user._id) {
+    if (payload.id !== user._id.toString) {
       return res.status(403).send("You can delete only your account!");
     }
     await User.findByIdAndDelete(req.params.id);
