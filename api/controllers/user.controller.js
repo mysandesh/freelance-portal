66924022d2cf415621900a70c0f46f4d745c7d64
Sandesh Acharya <mysandesh@gmail.com>
2023-04-20 22:34:55 +0545

    User credential credential string conversion bug fix

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,9 +7,10 @@ export const deleteUser = async (req, res) => {
   if (!token) return res.status(401).send("You are not authenticated!");
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    if (payload.id !== user._id.toString) {
+    if (payload.id !== user._id.toString()) {
       return res.status(403).send("You can delete only your account!");
     }
     await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("Deleted.");
   });
 };
