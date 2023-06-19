2fd0a393e8b853696b55cdcd136e6f655d92e737
Sandesh Acharya <mysandesh@gmail.com>
2023-06-19 14:11:36 +0545

    Authentication failure message configured

--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -1,4 +1,4 @@
 export const verifyToken = (req, res) => {
   const token = req.cookies.accessToken;
-  if (!token) return res.status(401).send("You are not authenticated");
+  if (!token) return res.status(401).send("You are not authenticated.");
 };
