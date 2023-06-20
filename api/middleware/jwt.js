47e25a485e9756c46d93d934a43db201bf008d62
Sandesh Acharya <mysandesh@gmail.com>
2023-06-20 17:03:47 +0545

    JWT Middleware configured

--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -1,4 +1,9 @@
 export const verifyToken = (req, res) => {
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).send("You are not authenticated.");
+
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    req.userId = payload.id;
+    req.isSeller = payload.isSeller;
+  });
 };
