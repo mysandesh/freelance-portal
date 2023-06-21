4b6350bfe9de93b6277903e85420e99b4c123a59
Sandesh Acharya <mysandesh@gmail.com>
2023-06-21 22:03:10 +0545

    Error Handling in Express Server initiated

--- api/server.js
+++ api/server.js
@@ -35,6 +35,12 @@ app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/reviews", reviewRoute);
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).send(errorMessage);
+});
+
 app.listen(8800, () => {
   connect();
   console.log("Backend server is running!");
