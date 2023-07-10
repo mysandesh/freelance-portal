6fa4650a4da7f2b0628ee638163908dc9712fb09
Sandesh Acharya <mysandesh@gmail.com>
2023-07-10 22:19:58 +0545

    CORS middleware added on server

--- api/server.js
+++ api/server.js
@@ -9,6 +9,7 @@ import conversationRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -24,6 +25,7 @@ const connect = async () => {
   }
 };
 
+app.use(cors({ origin: "http://localhost:5174", credential: true }));
 app.use(express.json());
 app.use(cookieParser());
 
