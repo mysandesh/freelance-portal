0dee376834e715efb83830cc5e13bd94078d68dc
Sandesh Acharya <mysandesh@gmail.com>
2023-07-10 21:31:52 +0545

    Login Page User and Password fields added

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./Login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <form>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input name="username" type="text" placeholder="user" />
+        <label htmlFor="">Password</label>
+        <input name="password" type="password" placeholder="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
