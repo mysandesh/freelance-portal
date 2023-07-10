f0e59cae04a85e710aa9d0f86fb2673016787c44
Sandesh Acharya <mysandesh@gmail.com>
2023-07-10 22:01:53 +0545

    Login route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Gigs from "./pages/gigs/Gigs";
 import Gig from "./pages/gig/Gig";
+import Login from "./pages/login/Login";
 import Add from "./pages/add/Add";
 import Orders from "./pages/orders/Orders";
 import Messages from "./pages/messages/Messages";
@@ -59,6 +60,10 @@ function App() {
         {
           path: "/message/:id",
           element: <Message />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
         },
       ],
     },
