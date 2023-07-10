aab52f92cef9999cf274761ec4ac2341cd0c25b3
Sandesh Acharya <mysandesh@gmail.com>
2023-07-10 21:39:07 +0545

    useState hooks added on Login Page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import "./Login.scss";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
   return (
     <div className="login">
       <form>
         <h1>Sign in</h1>
         <label htmlFor="">Username</label>
-        <input name="username" type="text" placeholder="user" />
+        <input
+          name="username"
+          type="text"
+          placeholder="user"
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <label htmlFor="">Password</label>
-        <input name="password" type="password" placeholder="password" />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
       </form>
     </div>
