fba6911605a19d34b5bd6ce0f500d1a5c622434c
Sandesh Acharya <mysandesh@gmail.com>
2023-07-13 11:26:51 +0545

    Login cookie for Axios setup

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -10,10 +10,14 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8800/api/auth/login", {
-        username,
-        password,
-      });
+      const res = await axios.post(
+        "http://localhost:8800/api/auth/login",
+        {
+          username,
+          password,
+        },
+        { withCredentails: true }
+      );
       console.log(res.data);
     } catch (err) {
       setError(err);
