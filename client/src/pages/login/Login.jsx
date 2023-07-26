742c1b249c06bad91a563f9831be75927fe6b107
Sandesh Acharya <mysandesh@gmail.com>
2023-07-26 19:00:29 +0545

    useNavigate hook  added

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import "./Login.scss";
 import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const res = await newRequest.post("/auth/login", { username, password });
       localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/");
     } catch (err) {
       setError(err.response.data);
     }
