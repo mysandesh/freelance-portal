305814b22802d271e729f9d875974babe8f1886c
Sandesh Acharya <mysandesh@gmail.com>
2023-08-01 23:06:37 +0545

    Upload utility created

---
+++ client/src/utils/upload.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const upload = async (file) => {
+  const data = new FormData();
+  data.append("file", file);
+  data.append("upload_preset", "freelance-portal");
+
+  try {
+    const res = await axios.post(
+      "https://api.cloudinary.com/v1_1/sandeshacharya/image"
+    );
+
+    const { url } = res.data;
+    return url;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default upload;
